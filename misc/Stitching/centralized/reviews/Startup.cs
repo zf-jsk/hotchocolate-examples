72b7a96368949df9accd0d1f4c609b22470e9759
Vijayakumar K <11499@ramco.com>
2022-03-30 17:47:08 +0530

    #SampleCommit

--- misc/Stitching/centralized/reviews/Startup.cs
+++ misc/Stitching/centralized/reviews/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+//Sample Commit
 namespace Demo.Reviews
 {
     public class Startup
