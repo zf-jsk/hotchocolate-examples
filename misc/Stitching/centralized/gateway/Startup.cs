099e4457ca5dbd2dd065789d8af8000683a85055
Saravana Kumar J <saravanakj@zerofence.com>
2022-03-30 17:42:39 +0530

    runtime fix added

--- misc/Stitching/centralized/gateway/Startup.cs
+++ misc/Stitching/centralized/gateway/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.Net.Http;
 using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,19 +20,20 @@ namespace Demo.Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient(Accounts, c => c.BaseAddress = new Uri("http://localhost:5051/graphql"));
-            services.AddHttpClient(Inventory, c => c.BaseAddress = new Uri("http://localhost:5052/graphql"));
-            services.AddHttpClient(Products, c => c.BaseAddress = new Uri("http://localhost:5053/graphql"));
-            services.AddHttpClient(Reviews, c => c.BaseAddress = new Uri("http://localhost:5054/graphql"));
+            services.AddHttpClient(Accounts, c => c.BaseAddress = new Uri("http://localhost:5051/graphql")).AddFiddler(true);
+            services.AddHttpClient(Inventory, c => c.BaseAddress = new Uri("http://localhost:5052/graphql")).AddFiddler(true);
+            services.AddHttpClient(Products, c => c.BaseAddress = new Uri("http://localhost:5053/graphql")).AddFiddler(true);
+            services.AddHttpClient(Reviews, c => c.BaseAddress = new Uri("http://localhost:5054/graphql")).AddFiddler(true);
 
             services
                 .AddGraphQLServer()
-                .AddQueryType(d => d.Name("Query"))
-                .AddRemoteSchema(Accounts, ignoreRootTypes: true)
-                .AddRemoteSchema(Inventory, ignoreRootTypes: true)
-                .AddRemoteSchema(Products, ignoreRootTypes: true)
-                .AddRemoteSchema(Reviews, ignoreRootTypes: true)
+                //.AddQueryType(d => d.Name("Query"))
+                .AddRemoteSchema(Accounts, ignoreRootTypes: false)
+                .AddRemoteSchema(Inventory, ignoreRootTypes: false)
+                .AddRemoteSchema(Products, ignoreRootTypes: false)
+                .AddRemoteSchema(Reviews, ignoreRootTypes: false)
                 .AddTypeExtensionsFromFile("./Stitching.graphql");
+                // .InitializeOnStartup();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +50,21 @@ namespace Demo.Gateway
             {
                 endpoints.MapGraphQL();
             });
+        }
+    }
+
+    static class extn {
+        public static void AddFiddler(this IHttpClientBuilder builder, bool enableFiddlerDebugging)
+        {
+            if (enableFiddlerDebugging)
+                builder.ConfigurePrimaryHttpMessageHandler(() =>
+                {
+                    return new HttpClientHandler
+                    {
+                        Proxy = new WebProxy("http://localhost:8888"),
+                        UseProxy = true
+                    };
+                });
         }
     }
 }
