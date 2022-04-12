b788a957e6589a7ce21c61f176556b341dfb454a
vijayakumar K <vijayakumarkupusamy@gmail.com>
2022-04-12 13:41:48 +0530

    #Headers propogation from gateway to apis

--- misc/Stitching/apollo-gateway/fed1/apollofederation/index.js
+++ misc/Stitching/apollo-gateway/fed1/apollofederation/index.js
@@ -6,8 +6,16 @@ const supergraphSdl = readFileSync('./supergraph.graphql').toString();
 
 class AuthenticatedDataSource extends RemoteGraphQLDataSource {
   willSendRequest({ request, context }) {
+    const headers=context;
+    //console.log(context.req);
     // Pass the user's id from the context to each subgraph
     // as a header called `user-id`
+    for (const key in headers) {
+      const value = headers[key];
+      if (value) {
+          request.http?.headers.set(key, String(value));
+      }
+  }      
     request.http.headers.set('X-ContextUser-Id', context.userId);
   }
 }
@@ -24,11 +32,12 @@ const server = new ApolloServer({
 
   context: ({ req }) => {
     // Get the user token from the headers
-    const token = req.headers.authorization || '';
+    const token = req.headers || '';
+    //console.log(req.headers);
     // Try to retrieve a user with the token
     const userId = "Vijay";//getUserId(token);
     // Add the user ID to the context
-    return { userId };
+    return token ;
   },
 });
 
